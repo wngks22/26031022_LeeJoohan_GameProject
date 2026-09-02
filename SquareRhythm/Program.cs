8c5dffe72b9e1212442b6a0e3fb96e4fe3e09efa
wngks22 <joohan1822@gmail.com>
2026-09-02 14:49:37 +0900

    game library 적용

--- SquareRhythm/Program.cs
+++ SquareRhythm/Program.cs
@@ -1,10 +1,10 @@
 ﻿namespace SquareRhythm
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    //internal class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("Hello, World!");
+    //    }
+    //}
 }
