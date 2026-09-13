7c98946abbdb953500b9a2c5c87cd779f015fe3f
wngks22 <joohan1822@gmail.com>
2026-09-13 18:16:42 +0900

    3차과제

--- SquareRhythm/GameMain.cs
+++ SquareRhythm/GameMain.cs
@@ -9,25 +9,32 @@ class GameMain : G2AppBase
 	public override System.Drawing.Size ScreenSize => GameGlobal.ScreenSize;
 	public override string GameName => GameGlobal.GameName;
 
-	private G2Texture? _bgTexture = null;
-	private G2Texture? _uiTitleTexture = null;
-	private G2Texture? _uiButtonTexture = null;
-
-	private G2Font? _fntMessage = null;
-
+    private G2Font? _fntMessage1 = null;
+    private G2Font? _fntMessage2 = null;
+    private G2Texture? _bgTexture = null;
+    private G2Texture? _uiTitleTexture = null;
+    private G2Texture? _uiButtonTexture = null;
+    private G2Texture? _uiSelectedTexture = null;
+    private G2Texture? _grayNoteTexture = null;
+    private G2Texture? _redNoteTexture = null;
+    private G2Texture? _blueNoteTexture = null;
 
     protected override void Initialize()
 	{
 		//---------------------------------------
 		// 게임 관련 객체를 생성합니다.
 		//---------------------------------------
-		var texUiDir = "resource/image/ui/";
+		var texUiDir = "resource/image/";
 
-		_bgTexture = new(texUiDir+"background.png");
-        _uiTitleTexture = new(texUiDir + "menu/title.png");
-		_uiButtonTexture = new(texUiDir + "menu/button.png");
-
-		_fntMessage = new("resource/font/ChakraPetch-Bold", 42);
+        _fntMessage1 = new("Bahnschrift", 32);
+        _fntMessage2 = new("Bahnschrift", 32);
+        _bgTexture = new(texUiDir + "ui/background.png");
+        _uiTitleTexture = new(texUiDir + "ui/menu/title.png");
+        _uiButtonTexture = new(texUiDir + "ui/menu/button.png");
+        _uiSelectedTexture = new(texUiDir + "ui/menu/selected.png");
+        _grayNoteTexture = new(texUiDir + "note/gray/right.png");
+        _redNoteTexture = new(texUiDir + "note/red/right.png");
+        _blueNoteTexture = new(texUiDir + "note/blue/right.png");
     }
 
 	protected override void Update()
@@ -47,27 +54,39 @@ class GameMain : G2AppBase
 
 	protected override void Render()
 	{
-		//---------------------------------------
-		// 게임 관련 객체를 렌더링 합니다.
-		//---------------------------------------
+        //---------------------------------------
+        // 게임 관련 객체를 렌더링 합니다.
+        //---------------------------------------
+        
+        _bgTexture.Draw(0f, 0f);
+        _uiTitleTexture.Draw(240, 70);
+        _grayNoteTexture.Draw(390, 250);
+        _redNoteTexture.Draw(544, 250);
+        _blueNoteTexture.Draw(697, 250);
+        _uiButtonTexture.Draw(430, 400);
+        _uiButtonTexture.Draw(430, 500);
+        _uiSelectedTexture.Draw(350, 400);
 
-		_bgTexture.Draw();
-		_uiTitleTexture.Draw(257.2f, 84.3f);
-		_uiButtonTexture.Draw();
-
-		_fntMessage.DrawText("SCORE", new(20, 20, 500, 100), new(0.0f, 1.0f, 0.0f, 1.0f));
+        _fntMessage1.DrawText("START GAME", new(547, 435, 500, 100), new(1, 1, 1, 1));
+        _fntMessage2.DrawText("EXIT", new(609, 535, 500, 100), new(0.7f, 0.7f, 0.7f, 1));
     }
 
 	public override void Dispose()
 	{
 		base.Dispose();
-		//---------------------------------------
-		// 게임 관련 객체를 해제합니다.
-		//---------------------------------------
-		_bgTexture.Dispose();
-		_uiTitleTexture.Dispose();
-		_uiButtonTexture.Dispose();
+        //---------------------------------------
+        // 게임 관련 객체를 해제합니다.
+        //---------------------------------------
+       
+        _bgTexture.Dispose();
+        _uiTitleTexture.Dispose();
+        _uiButtonTexture.Dispose();
+        _uiSelectedTexture.Dispose();
+        _grayNoteTexture.Dispose();
+        _redNoteTexture.Dispose();
+        _blueNoteTexture.Dispose();
 
-		_fntMessage.Dispose();
+        _fntMessage1.Dispose();
+        _fntMessage2.Dispose();
     }
 }
